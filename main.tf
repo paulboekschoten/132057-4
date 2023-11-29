6f46db1dff2b56cdba13c85d7b0f2d37089d58b4
Paul Boekschoten <paul.boekschoten@hashicorp.com>
2023-11-29 14:33:02 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,20 @@
 resource "null_resource" "test" {
 }
+
 resource "null_resource" "test2" {
+}
+
+resource "null_resource" "test3" {
+}
+
+resource "null_resource" "test4" {
+}
+
+resource "null_resource" "test5" {
+}
+
+resource "null_resource" "test6" {
+}
+
+resource "null_resource" "test7" {
 }
